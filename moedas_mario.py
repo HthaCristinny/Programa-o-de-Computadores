edd8bd8d18b884152477d191d0ab6bf2fc4a5b87
HthaCristinny <163015484+HthaCristinny@users.noreply.github.com>
2025-05-12 21:04:33 -0300

    Create moedas_mario.py

---
+++ moedas_mario.py
@@ -0,0 +1,4 @@
+bloco1 = int(input("Quantas moedas Mario pegou no primeiro bloco? "))
+bloco2 = int(input("E no segundo bloco? "))
+total = bloco1 + bloco2
+print("💰 Total de moedas coletadas:", total)
